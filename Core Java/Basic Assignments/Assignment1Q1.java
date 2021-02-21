e11281a610d609ef8fc94de487c5fadf40422a5f
Ashik <ashikpal531@gmail.com>
2021-02-22 00:22:13 +0530

    First Commit

---
+++ Core Java/Basic Assignments/Assignment1Q1.java
@@ -0,0 +1,37 @@
+//package JavaFiles;
+
+import java.util.Scanner;
+
+class ArmstrongOrNot {
+    public boolean armstrongCheck(int num) {
+    	int number=num;
+    	int rem,result=0,count=0,num1=num;
+    	while (num1 != 0) {
+    	      num1 /= 10;
+    	      ++count;
+    	    }
+    	while(num!=0)
+    	{
+    		rem=num%10;
+    		result+=Math.pow(rem, count);
+    		num/=10;
+    	}
+    	if(number==result)
+    		return true;
+    	else
+    		return false;
+    }
+
+}
+
+public class Assignment1Q1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int number = scan.nextInt();
+		ArmstrongOrNot a = new ArmstrongOrNot();
+		System.out.print(a.armstrongCheck(number)); 
+
+	}
+
+}
